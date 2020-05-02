277ea7e499b5945c436e6b9a47bc3b336c48b599
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-02 15:05:43 +0300

    Add project files.

---
+++ DialogService.Linux/LinuxDialogService.cs
@@ -0,0 +1,18 @@
+﻿using DialogService.Items;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DialogService.Linux
+{ 
+    /// <summary>
+    /// Dialog service implementation for Linux
+    /// </summary>
+    public class LinuxDialogService : IDialogService
+    {
+        public void Show(Dialog dialog)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DialogService.Linux/Platform.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DialogService
+{
+    /// <summary>
+    /// Functionality to get Linux platform implementation of dialog service
+    /// </summary>
+    public class PlatformImplementation : AbstractPlatform
+    {
+        /// <summary>
+        /// Gets Linux dialog service implementation
+        /// </summary>
+        /// <returns></returns>
+        public override IDialogService Get() => new Linux.LinuxDialogService();
+    }
+}
